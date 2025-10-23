1ed3429f923e8aecfbf856563305f6d4626f8ff2
gab <gabrielasofia.bcc@gmail.com>
2025-10-23 14:09:20 -0300

    Create bubbleflagsort.java

---
+++ bubbleflagsort.java
@@ -0,0 +1,28 @@
+public class bubbleflagsort implements sorter {
+
+    @Override
+    public String name() { return "bubble sort (flag)"; }
+
+    @Override
+    public sortmetrics sort(int[] v) {
+        sortmetrics m = new sortmetrics();
+        int n = v.length;
+        boolean trocou = true;
+
+        for (int pass = 0; pass < n - 1 && trocou; pass++) {
+            m.addI(1);
+            trocou = false;
+            for (int i = 0; i < n - 1 - pass; i++) {
+                m.addI(1);
+                if (v[i] > v[i + 1]) {
+                    int tmp = v[i];
+                    v[i] = v[i + 1];
+                    v[i + 1] = tmp;
+                    m.addT(1);
+                    trocou = true;
+                }
+            }
+        }
+        return m;
+    }
+}
